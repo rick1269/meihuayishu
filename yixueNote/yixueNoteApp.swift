c068b6f50983c1c1da532495412a834d56749e07
qzb <qzb@meitu.com>
2024-06-29 02:04:04 +0800

    新建文件

---
+++ yixueNote/yixueNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  yixueNoteApp.swift
+//  yixueNote
+//
+//  Created by rick qiu on 2024/6/29.
+//
+
+import SwiftUI
+
+@main
+struct yixueNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
